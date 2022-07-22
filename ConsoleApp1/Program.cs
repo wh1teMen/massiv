f98fc01061839fc247a1463eedc92486cb36bbf2
iterin <88264634+iterin@users.noreply.github.com>
2022-07-22 20:17:48 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp1
+{    class Program
+
+
+    {
+        static void Main(string[] args)
+        {
+    
+           Uchastniki[] uchastniki = new Uchastniki[3];
+           for (int i = 0; i < uchastniki.Length; i++)
+            {
+                uchastniki[i] = new Uchastniki(i+1);
+            }
+
+
+            Console.WriteLine("\nВсе участники: ");
+            foreach (var item in uchastniki)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            Console.WriteLine("[1] Добавить участника ");
+            switch (Console.ReadLine())
+            {
+                    case "1":
+                    Array.Resize(ref uchastniki, uchastniki.Length + 1);
+                    uchastniki[uchastniki.Length - 1] = new Uchastniki(uchastniki.Length); break;
+                    default: break;
+            }     
+
+            
+
+        }
+    }
+}
+
---
+++ ConsoleApp1/Uchastniki.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic; //колекции
+using System.Linq; // сортировка и работа с массивом
+using System.Text; //для работы с string
+using System.Threading.Tasks;//многопоточность
+namespace ConsoleApp1
+{//участники соревнований по спортивному ориентрованию
+
+    class Uchastniki  
+    {
+        public Uchastniki(int id, string name,  int age)
+        {
+            Id = id;
+            Name = name;
+          
+            Age = age;
+        }
+        public Uchastniki(int id)
+        {
+            Id = id;
+            Name =vod("Введите ФИО участника:  ");
+
+            Age =vod_age("Введите фозраст участника:  ");
+        }
+        
+        string vod(string text)
+        {
+            Console.Write(text);
+            return Console.ReadLine();
+        }
+        int vod_age(string text)
+        {
+            try
+            {
+                Console.Write(text);
+                return int.Parse(Console.ReadLine());
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("что то пошло не так");
+               return  vod_age(text);
+                
+            }
+        }
+
+
+
+        //свойства
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return $"ID Участника: { Id } \nИмя: { Name }\nВозраст: {Age}\n" ;
+        }
+
+
+
+    }
+}
